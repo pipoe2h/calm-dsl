81b5de05b96952fb05ac93d6b1d13b58f077b9df
Jose Gomez <pipoe2h@gmail.com>
2020-05-04 18:58:54 +0000

    Update: ignore vars

--- blueprints/vm-mobility/scripts/shell/move_deploy.sh
+++ blueprints/vm-mobility/scripts/shell/move_deploy.sh
@@ -8,6 +8,6 @@ docker run --rm --name calm-dsl \
     /bin/bash -c " \
     calm create bp -n @@{calm_application_name}@@-bp-move-vApp \
     -f vm-mobility/bps/move-ahv-appliance.py; \
-    calm launch bp \
+    calm launch bp -i \
     -a @@{calm_application_name}@@-app-move-vApp \
     @@{calm_application_name}@@-bp-move-vApp"
