a5a480a78395d427679306a5792c3b145a71e1e2
Jose Gomez <pipoe2h@gmail.com>
2020-05-09 22:56:42 +0000

    Update: fixing key

--- blueprints/vm-mobility/bps/scripts/move_monitor_migration_plan.py
+++ blueprints/vm-mobility/bps/scripts/move_monitor_migration_plan.py
@@ -31,7 +31,7 @@ if r.ok:
     
     while "Progress" not in resp['Status']['VMStatus'][0]:
 
-        if "RETRY" in resp['Status']['VMStatus'][0]['Actions'][0]:
+        if "RETRY" in resp['Status']['VMStatus'][0]:
             print(resp['Status']['VMStatus'][0]['ErrorMessage'])
             exit(1)
 
